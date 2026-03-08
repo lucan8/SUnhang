ac1df2792fad5d16cf45d5bec6b3425416e5fffb
lucan8 <gamer.lucan72@gmail.com>
2026-03-08 13:11:34 +0200

    Defined Event. Using it instead of vector clocks for dependencies and critical history.

--- include/deadlock_checker.hpp
+++ include/deadlock_checker.hpp
@@ -6,4 +6,6 @@ struct DeadlockChecker{
     bool is_dlk(const NodeChainT& cycle);
     bool is_abs_dlk_pattern(const NodeChainT& cycle);
     bool is_sync_preserving_dlk(const NodeChainT& cycle);
+
+    
 };
